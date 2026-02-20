060a70169c0ee1dca53866d84cca22848f88b260
Daniel Mursa <daniel@maykinmedia.nl>
2026-02-20 12:11:40 +0100

    :sparkles: [#28] Add check-envvar-docs action and open-api-ci actions

--- src/opendms/conf/ci.py
+++ src/opendms/conf/ci.py
@@ -1,35 +1,43 @@
+"""
+Continuous integration settings module.
+"""
+
 import os
 import warnings
 
-os.environ.setdefault("DEBUG", "yes")
-os.environ.setdefault("ALLOWED_HOSTS", "*")
-os.environ.setdefault(
-    "SECRET_KEY",
-    "django-insecure-kex=ipoau_q(o_w=6bd8q6_sb2#xgkdbknq4m&hl2jtx(7#$n_",
-)
+# Importing the idna module has an IO side-effect to load the data, which is a rather
+# big file. Pre-loading this in the settings file populates the python module cache,
+# preventing flakiness in hypothesis tests that hit this code path.
+import idna  # noqa: F401
+from open_api_framework.conf.utils import mute_logging
+
 os.environ.setdefault("IS_HTTPS", "no")
-os.environ.setdefault("VERSION_TAG", "dev")
-
-os.environ.setdefault("DB_NAME", "opendms")
-os.environ.setdefault("DB_USER", "opendms")
-os.environ.setdefault("DB_PASSWORD", "opendms")
-
-os.environ.setdefault("ENVIRONMENT", "development")
+os.environ.setdefault("SECRET_KEY", "dummy")
+# Do not log requests in CI/tests:
+#
+# * overhead making tests slower
+# * it conflicts with SimpleTestCase in some cases when the run-time configuration is
+#   looked up from the django-solo model
+os.environ.setdefault("LOG_REQUESTS", "no")
 os.environ.setdefault("OTEL_SDK_DISABLED", "true")
+os.environ.setdefault("OTEL_SERVICE_NAME", "opendms-ci")
 
 from .base import *  # noqa isort:skip
-from .base import CACHES
 
 CACHES.update(
     {
-        "default": {
-            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-            "LOCATION": "default",
-        },
+        "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"},
         # See: https://github.com/jazzband/django-axes/blob/master/docs/configuration.rst#cache-problems
         "axes": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"},
+        "oidc": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"},
     }
 )
+
+# shut up logging
+mute_logging(LOGGING)
+
+# don't spend time on password hashing in tests/user factories
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.PBKDF2PasswordHasher"]
 
 ENVIRONMENT = "CI"
 
@@ -47,3 +55,5 @@ warnings.filterwarnings(
     RuntimeWarning,
     r"django\.db\.models\.fields",
 )
+
+NOTIFICATIONS_DISABLED = True
