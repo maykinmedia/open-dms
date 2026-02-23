5c0d25cde4b9d3eeeeffe7866f6dbd438e348b81
Sven van de Scheur <svenvandescheur@gmail.com>
2026-02-23 13:11:32 +0100

    :white_check_mark: test: add Storybook

--- src/opendms/frontend/eslint.config.js
+++ src/opendms/frontend/eslint.config.js
@@ -1,17 +1,15 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { ignoreBuildArtifacts } from "@maykinmedia/eslint-config";
 import maykin from "@maykinmedia/eslint-config/recommended";
 
-const config = [
-  ignoreBuildArtifacts(["build"]),
-  {
-    ignores: ["dist"],
+const config = [ignoreBuildArtifacts(["build"]), {
+  ignores: ["dist"],
+}, ...maykin, {
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
   },
-  ...maykin,
-  {
-    rules: {
-      "react-hooks/exhaustive-deps": "off",
-    },
-  },
-];
+}, ...storybook.configs["flat/recommended"]];
 
 export default config;
--- src/opendms/frontend/vite.config.ts
+++ src/opendms/frontend/vite.config.ts
@@ -1,23 +1,51 @@
+/// <reference types="vitest/config" />
 // eslint-disable-next-line import/no-unresolved
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   build: {
-    assetsDir: "static", // Make sure Django can pick up assets.
+    assetsDir: "static" // Make sure Django can pick up assets.
   },
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  plugins: [react({
+    babel: {
+      plugins: [["babel-plugin-react-compiler"]]
+    }
+  })],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "src"),
-    },
+      "~": path.resolve(__dirname, "src")
+    }
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
 });
