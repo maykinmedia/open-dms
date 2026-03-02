672daa67c00e2e9a2492feac00fe555741cc7194
Sven van de Scheur <svenvandescheur@gmail.com>
2026-03-02 12:56:19 +0100

    :art: [#17] refactor: cleanup service select

--- src/opendms/frontend/src/components/service-select/service-select.tsx
+++ src/opendms/frontend/src/components/service-select/service-select.tsx
@@ -1,20 +1,39 @@
-import { type LoadOptionsFn, Select } from "@maykin-ui/admin-ui";
+import { type LoadOptionsFn, type Option, Select } from "@maykin-ui/admin-ui";
 import { useNavigate, useParams } from "react-router";
-
-type ServiceSelectProps = {
-  options: LoadOptionsFn;
-};
+import { apiClient } from "~/lib";
 
 /**
  * Allows selection of a service, powered by a loader function.
  * Updates the route to `/:service_slug` upon selection
- * @param options
  */
-export function ServiceSelect({ options }: ServiceSelectProps) {
+export function ServiceSelect() {
   const navigate = useNavigate();
-
   // TODO: Validation. See issue gh-#42
   const { service_slug } = useParams() as { service_slug: string | undefined };
+
+  /**
+   * Fetches the service options from the backend (client-side)
+   * Accepts string as the first parameter according to `@maykin-ui / LoadOptionsFn` and returns a Promise of results
+   * @param search
+   */
+  const getServiceOptions: LoadOptionsFn = async (
+    search,
+  ): Promise<Option[]> => {
+    const { data } = await apiClient.GET("/api/v1/services", {
+      params: {
+        query: {
+          search,
+        },
+      },
+    });
+
+    return (
+      data?.results.map(({ label, slug }) => ({
+        label,
+        value: slug,
+      })) || []
+    );
+  };
 
   /**
    * Routes to `:service_slug`
@@ -26,10 +45,10 @@ export function ServiceSelect({ options }: ServiceSelectProps) {
 
   return (
     <Select
-      options={options}
-      placeholder="Selceteer Service"
-      placeholderSearch="Zoeken"
       labelNoOptions="Geen resultaten"
+      options={getServiceOptions}
+      placeholder="Selecteer service"
+      placeholderSearch="Zoeken"
       value={service_slug}
       variant="secondary"
       onChange={({ target }) => setSelectedService(target.value)}
