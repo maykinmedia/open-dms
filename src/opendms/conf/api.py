8917c9b7cdf0479572f36115d3b238a9cee6b6a2
Daniel Mursa <daniel@maykinmedia.nl>
2026-03-13 11:52:00 +0100

    :sparkles: [#18] Add custom pagination

--- src/opendms/conf/api.py
+++ src/opendms/conf/api.py
@@ -7,7 +7,7 @@ REST_FRAMEWORK = BASE_REST_FRAMEWORK.copy()
 REST_FRAMEWORK["PAGE_SIZE"] = 100
 
 REST_FRAMEWORK["DEFAULT_PAGINATION_CLASS"] = (
-    "vng_api_common.pagination.DynamicPageSizePagination"
+    "opendms.api.utils.pagination.CountedPagination"
 )
 REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = (
     "rest_framework.authentication.SessionAuthentication",
