aae339d9cb001de304a47aed6822a4d4d95f1e89
Tim de Beer <tim@maykinmedia.nl>
2026-03-27 16:31:29 +0100

    :recycle: [#57] make use of client and added functions

--- src/opendms/api/clients/documenten.py
+++ src/opendms/api/clients/documenten.py
@@ -93,11 +93,6 @@ class DocumentClient(HttpRequestMixin, NLXClient):
             bestandsomvang=record["bestandsomvang"],
         )
 
-    def retrieve(self, uuid: str) -> dict:
-        url = f"{self.endpoint}/{uuid}"
-        response = self.make_request(url)
-        return response
-
 
 class ObjectInformatieObjectClient(HttpRequestMixin, NLXClient):
     """
