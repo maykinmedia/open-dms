d506e247c7f26b8194588623a4369ffab6ab726a
Sven van de Scheur <svenvandescheur@gmail.com>
2026-04-13 17:55:09 +0200

    :bug: fix: fix incorrect exception handling

--- src/opendms/api/viewsets.py
+++ src/opendms/api/viewsets.py
@@ -447,9 +447,11 @@ class DocumentViewSet(ReadOnlyViewSetMixin, viewsets.ViewSet):
             edit_file_url = document_edit_backend.open(tmp_path, file_uuid, file_ext)
             return redirect(edit_file_url)
         except Exception as exc:
-            response = getattr(exc, "response", None)
-            if response and response.status_code == status.HTTP_401_UNAUTHORIZED:
-                return self._redirect_to_ms_auth(request)
+            try:
+                if exc.response.status_code == status.HTTP_401_UNAUTHORIZED:
+                    return self._redirect_to_ms_auth(request)
+            except AttributeError:
+                pass
 
             logger.exception("Document upload to Drive failed")
             raise MsGraphApiBackendError(_("Document upload to Drive failed.")) from exc
