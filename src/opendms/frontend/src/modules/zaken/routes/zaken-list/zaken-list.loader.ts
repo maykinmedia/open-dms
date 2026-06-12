c7179110171ff8d0e944dc323f0efe54fa903e1d
Sven van de Scheur <svenvandescheur@gmail.com>
2026-06-12 12:53:05 +0200

    :sparkles: [#159] feat: add status expand to zaken zakenListLoader

--- src/opendms/frontend/src/modules/zaken/routes/zaken-list/zaken-list.loader.ts
+++ src/opendms/frontend/src/modules/zaken/routes/zaken-list/zaken-list.loader.ts
@@ -1,15 +1,9 @@
 import { invariant } from "@maykin-ui/client-common";
 import type { LoaderFunctionArgs } from "react-router";
+import * as z from "zod";
 import { apiClient } from "~/lib";
-import type { Zaak } from "~/types";
 
-export async function zakenListLoader({
-  request,
-  params,
-}: LoaderFunctionArgs): Promise<{
-  count: number;
-  results: Zaak[];
-} | null> {
+export async function zakenListLoader({ request, params }: LoaderFunctionArgs) {
   if (!params.serviceSlug) return null;
 
   const url = new URL(request.url);
@@ -26,6 +20,7 @@ export async function zakenListLoader({
             urlSearchParams.get("identificatie__icontains") || undefined,
           einddatum__isnull: true,
           ordering: "-startdatum",
+          expand: "status",
         },
       },
     },
@@ -33,5 +28,18 @@ export async function zakenListLoader({
 
   if (!response.ok) throw new Error(response.statusText);
   invariant(data, "Failed to load data!");
-  return data;
+
+  // Expand is typed Record<string, never> as the schema is dynamic.
+  // We use zod to parse the actual value and provide the proper result schema.
+  const results = data.results.map((zaak) => {
+    const StatusExpand = z.object({
+      status: z.object({
+        statustype: z.url(),
+      }),
+    });
+    const _expand = z.parse(StatusExpand, zaak._expand);
+    return { ...zaak, _expand };
+  });
+
+  return { ...data, results };
 }
