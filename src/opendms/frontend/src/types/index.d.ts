fe1cacae12b254079c845c4ad81d64bf838bc2ba
Sven van de Scheur <svenvandescheur@gmail.com>
2026-06-12 12:51:57 +0200

    :label: [#159] chore: add StatusType to types and sanitize Zaak type

--- src/opendms/frontend/src/types/index.d.ts
+++ src/opendms/frontend/src/types/index.d.ts
@@ -7,8 +7,13 @@ export type User = components["schemas"]["User"];
 
 export type Document = components["schemas"]["Document"];
 
+export type StatusType = components["schemas"]["StatusType"];
+
 export type ZaakType = components["schemas"]["ZaakType"];
-export type Zaak = components["schemas"]["Zaak"];
+
+// Deliberately remove _expand from schema as there is no sensible type for the
+// dynamic nature. Whenever used: _expand should be parsed to obtain the type.
+export type Zaak = Omit<components["schemas"]["Zaak"], "_expand">;
 
 export type Fout = components["schemas"]["Fout"];
 export type ValidatieFout = components["schemas"]["ValidatieFout"];
